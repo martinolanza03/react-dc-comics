c4c6a29783173f0bfcea1de9c4799eb27cc6177d
martinolanza03 <lanzamartino3@gmail.com>
2025-04-03 16:08:15 +0200

    feat: import font family, create a menu

--- src/components/BaseHeader.jsx
+++ src/components/BaseHeader.jsx
@@ -1,9 +1,11 @@
 import Logo from './BaseLogo.jsx';
+import Menu from './Menu.jsx'
 
 export default function Header() {
     return (
         <header>
             <Logo />
+            <Menu />
         </header>
     );
 }
---
+++ src/components/Menu.jsx
@@ -0,0 +1,16 @@
+export default function Menu() {
+    return (
+        <ul class="menu">
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">COMICS</a></li>
+            <li><a href="#">MOVIES</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">GAMES</a></li>
+            <li><a href="#">COLLECTIBLES</a></li>
+            <li><a href="#">VIDEOS</a></li>
+            <li><a href="#">FANS</a></li>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">SHOP</a></li>
+        </ul>
+    );
+}
