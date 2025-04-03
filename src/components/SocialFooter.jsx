edc3ca0844015a9e77001aadca3bed10fa113a31
martinolanza03 <lanzamartino3@gmail.com>
2025-04-03 18:37:39 +0200

    feat: push SocialFooter.jsx

---
+++ src/components/SocialFooter.jsx
@@ -0,0 +1,22 @@
+export default function SocialFooter() {
+    return (
+        <div class="container ">
+            <div class='footer-social'>
+                <div class="footer-content">
+                    <div class="footer-section">
+                        <button class="signup-btn">SIGNUP NOW</button>
+                    </div>
+                    <div class="footer-section">
+                        <h3 class="follow-title">FOLLOW US</h3>
+                        <div class="social-icons">
+                            <a href="#"><img src="../public/img/footer-facebook.png" alt="Facebook" /></a>
+                            <a href="#"><img src="../public/img/footer-twitter.png" alt="Twitter" /></a>
+                            <a href="#"><img src="../public/img/footer-youtube.png" alt="Youtube" /></a>
+                            <a href="#"><img src="../public/img/footer-pinterest.png" alt="Pinterest" /></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
