cc7bb31284381cfdcfe076b1c332a238d5115710
martinolanza03 <lanzamartino3@gmail.com>
2025-04-03 18:37:24 +0200

    feat: create SocialFooter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 import FooterMenu from './FooterMenu.jsx';
+import SocialFooter from './SocialFooter.jsx'
 
 export default function Footer() {
     return (<footer>
         <FooterMenu />
+        <SocialFooter />
     </footer>);
 }
