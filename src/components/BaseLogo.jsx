5dd2ef1e7b48a027a2d32f494dacc0037f6911d2
martinolanza03 <lanzamartino3@gmail.com>
2025-04-03 16:10:08 +0200

    feat: change link logo png

--- src/components/BaseLogo.jsx
+++ src/components/BaseLogo.jsx
@@ -1,5 +1,5 @@
 export default function Logo() {
     return (
-        <img src="../public/img/dc-logo-bg.png" alt="dc-logo" class='header-logo' />
+        <img src="../public/img/dc-logo.png" alt="dc-logo" class='header-logo' />
     );
 }
