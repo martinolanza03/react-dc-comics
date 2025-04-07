45fcee02517e0ba9cc6a69c78763014c5569d12d
martinolanza03 <lanzamartino3@gmail.com>
2025-04-07 15:56:19 +0200

    feat: current series button

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,9 @@ import Content from './Content.jsx';
 export default function Main() {
     return (<main>
         <img src="../img/jumbotron.jpg" alt="Jumbotron" className='jumbotron' />
+
         <div className='container'>
+            <button className='current-series-btn'>Current Series</button>
             <Content />
         </div>
 
