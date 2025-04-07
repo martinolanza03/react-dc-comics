2218605593193923cdd53d31a87917b815219db4
martinolanza03 <lanzamartino3@gmail.com>
2025-04-07 15:37:05 +0200

    feat: image error handled

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,12 +3,14 @@ import Comics from '../assets/comics.js';
 export default function Content() {
     return (
         <div className='content'>
-            <ul>
-                <li>ciao</li>
-                <li>ciao</li>
-                <li>ciao</li>
-                <li>ciao</li>
-            </ul>
-        </div>
+            <ul className='comics'>
+                {Comics.map(comic => {
+                    return comic.thumb !== undefined ? <li>
+                        <img src={comic.thumb} alt={comic.series} />
+                        <p>{comic.title}</p>
+                    </li> : '';
+                })}
+            </ul >
+        </div >
     );
 }
